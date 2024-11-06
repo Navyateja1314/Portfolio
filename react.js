79c81cb5238ebcf7995afeb0d11430ba9afe2954
Navyateja1314 <navyatejabandameedi@gmail.com>
2024-11-07 01:15:25 +0530

    update changes

--- react.js
+++ react.js
@@ -30,3 +30,25 @@ document.addEventListener("DOMContentLoaded", function () {
     toggleScrollBtn();
   });
   
+  
+    (function() {
+        
+        emailjs.init({
+          publicKey: "79Cjdy-vIptSNil5x",
+        });
+    })();
+
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // these IDs from the previous steps
+      emailjs.sendForm('contact_service', 'contact_form', this)
+          .then(() => {
+              console.log('SUCCESS!');
+          }, (error) => {
+              console.log('FAILED...', error);
+          });
+  });
+}
+    
